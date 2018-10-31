e0047afbc560bb241dd50751689fe22ce227da44
Gracie <graceliu@wustl.edu>
2018-10-31 10:36:42 -0500

    scroll v9

---
+++ Scroll/script.js
@@ -0,0 +1,40 @@
+$(document).ready(function() {
+	var a = Math.round(Math.random() * 255);
+	var b = Math.round(Math.random() * 255);
+	var c = Math.round(Math.random() * 255);
+	$("#set1 .m1").attr("data-0","background-color: rgb("+a+","+b+","+c+");" )
+	$("#set1 .m2").attr("data-10","background-color: rgb("+a+","+b+","+c+");" )
+	$("#set1 .m3").attr("data-20p","background-color: rgb("+a+","+b+","+c+");" )
+	$("#set1 .m4").attr("data-30p","background-color: rgb("+a+","+b+","+c+");" )
+	$("#set1 .m5").attr("data-40p","background-color: rgb("+a+","+b+","+c+");" )
+
+	$("#set2 .m1").attr("data-0","background-color: rgb("+a+","+b+","+c+");" )
+	$("#set2 .m2").attr("data-10","background-color: rgb("+a+","+b+","+c+");" )
+	$("#set2 .m3").attr("data-20p","background-color: rgb("+a+","+b+","+c+");" )
+	$("#set2 .m4").attr("data-30p","background-color: rgb("+a+","+b+","+c+");" )
+	$("#set2 .m5").attr("data-40p","background-color: rgb("+a+","+b+","+c+");" )
+
+	$("#set3 .m1").attr("data-0","background-color: rgb("+a+","+b+","+c+");" )
+	$("#set3 .m2").attr("data-10","background-color: rgb("+a+","+b+","+c+");" )
+	$("#set3 .m3").attr("data-20p","background-color: rgb("+a+","+b+","+c+");" )
+	$("#set3 .m4").attr("data-30p","background-color: rgb("+a+","+b+","+c+");" )
+	$("#set3 .m5").attr("data-40p","background-color: rgb("+a+","+b+","+c+");" )
+
+	$("#set4 .m1").attr("data-0","background-color: rgb("+a+","+b+","+c+");" )
+	$("#set4 .m2").attr("data-10","background-color: rgb("+a+","+b+","+c+");" )
+	$("#set4 .m3").attr("data-20p","background-color: rgb("+a+","+b+","+c+");" )
+	$("#set4 .m4").attr("data-30p","background-color: rgb("+a+","+b+","+c+");" )
+	$("#set4 .m5").attr("data-40p","background-color: rgb("+a+","+b+","+c+");" )
+
+	$("#set5 .m1").attr("data-0","background-color: rgb("+a+","+b+","+c+");" )
+	$("#set5 .m2").attr("data-10","background-color: rgb("+a+","+b+","+c+");" )
+	$("#set5 .m3").attr("data-20p","background-color: rgb("+a+","+b+","+c+");" )
+	$("#set5 .m4").attr("data-30p","background-color: rgb("+a+","+b+","+c+");" )
+	$("#set5 .m5").attr("data-40p","background-color: rgb("+a+","+b+","+c+");" )
+
+	$("#set6 .m1").attr("data-0","background-color: rgb("+a+","+b+","+c+");" )
+	$("#set6 .m2").attr("data-10","background-color: rgb("+a+","+b+","+c+");" )
+	$("#set6 .m3").attr("data-20p","background-color: rgb("+a+","+b+","+c+");" )
+	$("#set6 .m4").attr("data-30p","background-color: rgb("+a+","+b+","+c+");" )
+	$("#set6 .m5").attr("data-40p","background-color: rgb("+a+","+b+","+c+");" )
+})
